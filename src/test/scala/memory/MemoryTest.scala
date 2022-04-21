f36b6d6df378989c6f6c37889cd6da1dea0ab786
canoriz <83388275+canoriz@users.noreply.github.com>
2022-04-21 15:58:35 +0800

    Update test

--- src/test/scala/memory/MemoryTest.scala
+++ src/test/scala/memory/MemoryTest.scala
@@ -16,9 +16,9 @@ import Consts._
   * }}}
   */
 class MemoryTest extends AnyFlatSpec with ChiselScalatestTester {
-
-  "I-memory read m16k_1" should "pass" in {
-    test(new Memory("src/test/scala/resources/memory/m16k_1.memory")) { dut =>
+  val mem_path = "src/test/scala/resources/memory/m16k_1.memory"
+  "I-memory" should "read" in {
+    test(new Memory(mem_path)) { dut =>
       val expectRdata =Seq(0x6da82cadL, 0xe5d9d38fL, 0xda48fe6fL, 0x9a5e0e9fL).
         map(x => x.U(WORD_LEN.W))
       for (i <- 0 until 4) {
@@ -28,8 +28,8 @@ class MemoryTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
-  "D-memory read m16k_1" should "pass" in {
-    test(new Memory("src/test/scala/resources/memory/m16k_1.memory")) { dut =>
+  "D-memory" should "read" in {
+    test(new Memory(mem_path)) { dut =>
       val expectRdata =Seq(0x6da82cadL, 0xe5d9d38fL, 0xda48fe6fL, 0x9a5e0e9fL).
         map(x => x.U(WORD_LEN.W))
       for (i <- 0 until 4) {
@@ -40,8 +40,8 @@ class MemoryTest extends AnyFlatSpec with ChiselScalatestTester {
   }
 
 
-  "D-memory write m16k_1" should "pass" in {
-    test(new Memory("src/test/scala/resources/memory/m16k_1.memory")) { dut =>
+  "D-memory" should "write" in {
+    test(new Memory(mem_path)) { dut =>
       val writeData =Seq(0x01020304L, 0x05060708L, 0x090a0b0cL, 0xd0e0f000L).
         map(x => x.U(WORD_LEN.W))
       for (i <- 0 until 4) {
